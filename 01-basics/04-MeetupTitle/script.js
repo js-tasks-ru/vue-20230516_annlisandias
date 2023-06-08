a3c875b249064752d9d8417e9ed70f07847dfa2c
Hanna <annlisandias@gmail.com>
2023-06-08 18:06:17 +0200

    feat: Meetup Title

--- 01-basics/04-MeetupTitle/script.js
+++ 01-basics/04-MeetupTitle/script.js
@@ -1,17 +1,47 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
 const API_URL = 'https://course-vue.javascript.ru/api';
 
-function fetchMeetupById(meetupId) {
-  return fetch(`${API_URL}/meetups/${meetupId}`).then((response) => {
-    if (response.ok) {
-      return response.json();
-    } else {
-      return response.json().then((error) => {
-        throw error;
-      });
-    }
-  });
-}
-
 // Требуется создать Vue приложение
+
+createApp({
+  data() {
+      return {
+          meetupId: 1,
+          currentMeetupTitle: null
+      }
+  },
+  methods: {
+    fetchMeetupById(meetupId) {
+      return fetch(`${API_URL}/meetups/${meetupId}`)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          return response.json().then((error) => {
+            throw error;
+          });
+        }
+      })
+    }
+  },
+  watch: {
+    meetupId: {
+      handler(newValue, oldValue) {
+        if(newValue) {
+            this.fetchMeetupById(newValue).then((data) => {
+              this.currentMeetupTitle = data.title;
+            });
+        }
+      },
+      immediate: true
+    }
+  },
+  template: `
+    <div>
+      <label v-for="n in 5"><input v-model="meetupId" type="radio" name="meetupId" :value="n" /> {{ n }}</label>
+      <hr />
+      <h3>{{ currentMeetupTitle }}</h3>
+    </div>
+  `
+}).mount('#app');
