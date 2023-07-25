4ce561db60fa3d5c3371be579dab14e2266e2a90
Hanna Herasimava <annlisandias@gmail.com>
2023-07-25 22:58:39 +0200

    fix: meetup title

--- 02-components/03-MeetupCover/components/MeetupCover.js
+++ 02-components/03-MeetupCover/components/MeetupCover.js
@@ -23,6 +23,6 @@ export default defineComponent({
     <div class="meetup-cover" :style="coverStyle">
         <!-- Ссылка выше - пример верстки при наличии изображения, а не изображение по умолчанию. -->
         <!-- Изображение по умолчанию уже есть в стилях. Для его вывода достаточно не добавлять никаких стилей. -->
-        <h1 class="meetup-cover__title">Title</h1>
+        <h1 class="meetup-cover__title">{{ title }}</h1>
     </div>`,
 });
